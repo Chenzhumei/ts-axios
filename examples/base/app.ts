665f4cdd0ff150785c9fe6702fcb5ec1971dcc34
Chenzhumei <zcm_1018@163.com>
2022-04-08 11:29:58 +0800

    处理请求headers

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -100,13 +100,23 @@ import axios from '../../src/index'
 //   data: searchParams
 // })
 
-const arr = new Int32Array([21, 31])
+// const arr = new Int32Array([21, 31])
 
-axios({
-  method: 'post',
-  url: '/base/buffer',
-  data: arr
-})
+// axios({
+//   method: 'post',
+//   url: '/base/buffer',
+//   data: arr
+// })
+
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+
 
 axios({
   method: 'post',
@@ -115,4 +125,25 @@ axios({
     a: 1,
     b: 2
   }
+})
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
 })
