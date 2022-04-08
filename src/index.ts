b645b3abd23ace4b8f3e5f6298d6f2e00f514e39
Chenzhumei <zcm_1018@163.com>
2022-04-08 14:54:22 +0800

    处理响应data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { transformRequest } from './helpers/data';
+import { transformRequest, transformResponse } from './helpers/data';
 import { processHeaders } from './helpers/header';
 import { buildURL } from './helpers/url';
-import {AxiosRequestConfig, AxiosPromise} from './types';
+import {AxiosRequestConfig, AxiosPromise, AxiosResponse} from './types';
 import xhr from './xhr';
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config);
-  return xhr(config);
+  return xhr(config).then(res => transformResponseData(res));
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -27,6 +27,11 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig): any {
    const {headers = {}, data} = config;
    return processHeaders(headers, data);
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+   res.data = transformResponse(res.data);
+   return res;
 }
 
 export default axios;
