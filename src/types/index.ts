4f1e0f892b748fd059b3a998a7c98288a807644c
Chenzhumei <zcm_1018@163.com>
2022-04-09 22:39:17 +0800

    错误处理增强

--- src/types/index.ts
+++ src/types/index.ts
@@ -25,6 +25,13 @@ export interface AxiosResponse {
     request: any;
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {
+export interface AxiosPromise extends Promise<AxiosResponse> {}
 
+export interface AxiosError extends Error {
+    isAxiosError: boolean;
+    config: AxiosRequestConfig;
+    code?: string | null;
+    request?: any;
+    response?: AxiosResponse;
 }
+
